4b5cd1b63e8c38e5d5cd66fd0474335cc7962780
M I Y E O N <120808934+gimmecodeee@users.noreply.github.com>
2023-04-22 09:37:54 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php 
+
+    include 'connection.php';
+
+    session_start();
+
+    if(isset($_POST['login'])) {
+        $check = "SELECT * FROM `login-admin` WHERE ademail = '$_POST[email]'
+                AND adpassword = '$_POST[password]'";
+        
+        $result = mysqli_query($connection, $check);
+
+        if (mysqli_num_rows($result) == 1) {
+            header('location: adminpage.php');
+        }
+
+        else if (mysqli_num_rows($result) == 0) {
+            $check = "SELECT * FROM `login-user` WHERE email = '$_POST[email]'
+                AND password = '$_POST[password]'";
+
+            $result = mysqli_query($connection, $check);
+
+            if (mysqli_num_rows($result) == 1) {
+                header('location: userpage.php');
+        }
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aero Escapade | LOGIN</title>
+</head>
+
+<body>
+
+    <form action="" method="POST">
+
+            <input type="email" placeholder="Email Address" name="email">
+            <br><br>
+
+            <input id="pw" type="password" placeholder="Password" name="password">
+            <br><br>
+
+            <button id="login" type="submit" name="login">LOG IN</button>
+
+    </form>
+    
+</body>
+</html>
